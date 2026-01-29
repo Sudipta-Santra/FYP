16720464f5affe0a4df7f496d47578d546e0f9f7
Sudipta-Santra <sudiptasantra616@gmail.com>
2026-01-29 17:12:56 +0530

    integrate html with css and js

--- family_health/settings.py
+++ family_health/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)#0-vzus27_6t9_!#2qy1os6!^m8-v5zpmoceghh%=k=d01#!+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition --
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "home",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "family_health.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "family_health" / "template"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,11 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "css",
-    BASE_DIR / "js",
+    BASE_DIR / "family_health" / "static",
 ]
 
 # Default primary key field type
---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+def index(request):
+    return render(request, 'index.html')
+
+def health_report(request):
+    return render(request, 'health_report.html')
+
+def add_members(request):
+    return render(request, 'add_members.html')
+
+def medication(request):
+    return render(request, 'medication.html')
+
+def emergency(request):
+    return render(request, 'emergency.html')
+
+def ai_assistance(request):
+    return render(request, 'ai_assistance.html')
